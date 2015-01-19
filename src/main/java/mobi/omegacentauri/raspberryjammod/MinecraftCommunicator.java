340e3dd7fa9011b348fb4bb34a30f06892441490
arpruss <arpruss@gmail.com>
2015-01-19 10:26:11 -0600

    foo

---
+++ src/main/java/mobi/omegacentauri/raspberryjammod/MinecraftCommunicator.java
@@ -0,0 +1,555 @@
+package mobi.omegacentauri.raspberryjammod;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.IChatComponent;
+import net.minecraft.util.Vec3;
+import net.minecraft.util.Vec3i;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+
+public class MinecraftCommunicator {
+	// world.checkpoint.save/restore, player.setting, world.setting,
+	// camera.*, events.* unsupported
+	private static final String CHAT = "chat.post(";
+	public static final String SETBLOCK = "world.setBlock(";
+	public static final String SETBLOCKS = "world.setBlocks("; 
+	private static final String GETBLOCK = "world.getBlock(";
+	private static final String GETBLOCKWITHDATA = "world.getBlockWithData(";
+	private static final String GETHEIGHT = "world.getHeight("; 
+	private static final String GETPLAYERIDS = "world.getPlayerIds("; // singleplayer
+	private static final String GETPLAYERENTITYID = "world.getPlayerEntityId("; // singleplayer, only works for main player
+
+	private static final String PLAYERSETTILE = "player.setTile("; 
+	private static final String PLAYERSETPOS = "player.setPos("; 
+	private static final String PLAYERGETDIRECTION = "player.getDirection("; 
+	private static final String PLAYERGETROTATION = "player.getRotation("; 
+	private static final String PLAYERGETPITCH = "player.getPitch(";
+	private static final String PLAYERGETPOS = "player.getPos(";
+	private static final String PLAYERGETTILE = "player.getTile(";	
+	private static final String ENTITYGETDIRECTION = "entity.getDirection("; 
+	private static final String ENTITYGETROTATION = "entity.getRotation("; 
+	private static final String ENTITYGETPITCH = "entity.getPitch("; 
+	private static final String ENTITYGETPOS = "entity.getPos("; 
+	private static final String ENTITYGETTILE = "entity.getTile("; 
+	private static final String ENTITYSETTILE = "entity.setTile("; 
+	private static final String ENTITYSETPOS = "entity.setPos("; 
+
+	private static final Block UNKNOWN_BLOCK = Blocks.beacon;
+	Block[] typeMap;
+
+	private ServerSocket socket;
+	private DataOutputStream writer;
+	private World world;
+
+	public MinecraftCommunicator() throws IOException {
+		socket = new ServerSocket(4711);
+		initTypeMap();
+	}
+
+	void communicate() throws IOException {
+		String clientSentence;
+
+		while(true)
+		{
+			Socket connectionSocket = null;
+
+			try {
+				connectionSocket = socket.accept();
+				BufferedReader reader =
+						new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+				writer = new DataOutputStream(connectionSocket.getOutputStream());
+
+				while(null != (clientSentence = reader.readLine())) {
+					Scanner scan = null;
+					Minecraft mc = Minecraft.getMinecraft();
+					if (mc == null) {
+						sendLine("Error: minecraft not available");
+						continue;
+					}
+					world = MinecraftServer.getServer().getEntityWorld();
+					if (world == null) {
+						sendLine("Error: world not available");
+						continue;
+					}
+					EntityPlayerSP player = mc.thePlayer;
+					if (player == null) {
+						sendLine("Error: player not available");
+						continue;
+					}
+					process(mc, player, clientSentence);
+				}
+			}
+			catch (Exception e) {
+				System.out.println(""+e);
+				try {
+					Thread.sleep(1000);
+				}
+				catch(Exception e2) {					
+				}
+			}
+			finally {
+				if (connectionSocket != null)
+					connectionSocket.close();
+			}
+		}
+	}
+
+	private void process(Minecraft mc, EntityPlayerSP player,
+			String clientSentence) {
+		Scanner scan = null;
+
+		try {	
+			int args = clientSentence.indexOf('(');
+			if (args < 0)
+				return;
+			args++;
+			clientSentence = clientSentence.replaceAll("[\r\n)]+$", "");
+			scan = new Scanner(clientSentence.substring(args));
+			scan.useDelimiter(",");
+			if (clientSentence.startsWith(GETBLOCK)) {
+				Block block = world.getBlockState(getPosition(scan)).getBlock();
+				int id = getRaspberryType(block);
+				sendLine(""+id);
+			}
+			else if (clientSentence.startsWith(GETBLOCKWITHDATA)) {
+				IBlockState state = world.getBlockState(getPosition(scan));
+				Block block = state.getBlock();
+				int id = getRaspberryType(block);
+				sendLine(""+id+","+block.getMetaFromState(state));
+			}
+			else if (clientSentence.startsWith(GETHEIGHT)) {
+				BlockPos pos = getPosition(scan.nextInt(), 0, scan.nextInt());
+				Chunk chunk = world.getChunkFromBlockCoords(pos);
+				int h = chunk.getHeight(pos);
+				int x = pos.getX();
+				int z = pos.getZ();
+				for (int y = world.getHeight() ; y >= h ; y--) {
+					Block b = chunk.getBlock(x,y,z);
+					if (b != Blocks.air) {
+						h = y;
+						break;
+					}
+				}
+
+				h -= world.getSpawnPoint().getY();
+				
+				sendLine(h);
+			}
+			else if (clientSentence.startsWith(SETBLOCK)) {
+				BlockPos pos = getPosition(scan);
+				int type = scan.nextInt();
+
+				Block b = getBlockByRaspberryType(type);
+				try {
+					int data = scan.nextInt();
+					world.setBlockState(pos, b.getStateFromMeta(data), 2); // .getStateById(data), 2);
+				}
+				catch(Exception e) {
+					world.setBlockState(pos, b.getDefaultState(), 2);
+				}
+				world.checkLight(pos);
+				//		        if (!world.isRemote)
+				//		        {
+				//		            world.markBlockForUpdate(pos);
+				//		        }
+			}
+			else if (clientSentence.startsWith(SETBLOCKS)) {
+				BlockPos pos1 = getPosition(scan);
+				BlockPos pos2 = getPosition(scan);
+				int type = scan.nextInt();
+
+				Block b = getBlockByRaspberryType(type);
+				IBlockState state;
+				try {
+					int data = scan.nextInt();
+					state = b.getStateFromMeta(data);
+				}
+				catch(Exception e) {
+					state = b.getDefaultState();
+				}
+				int x1 = pos1.getX();
+				int x2 = pos2.getX();
+				if (x2 < x1) {
+					int t = x2;
+					x2 = x1;
+					x1 = t;
+				}
+				int y1 = pos1.getY();
+				int y2 = pos2.getY();
+				if (y2 < y1) {
+					int t = y2;
+					y2 = y1;
+					y1 = t;
+				}
+				int z1 = pos1.getZ();
+				int z2 = pos2.getZ();
+				if (z2 < z1) {
+					int t = z2;
+					z2 = z1;
+					z1 = t;
+				}
+				for (int x = x1; x <= x2; x++)
+					for (int y = y1; y <= y2; y++)
+						for (int z = z1 ; z <= z2; z++) {
+							BlockPos pos = new BlockPos(x,y,z);							
+							world.setBlockState(pos, state);
+							world.checkLight(pos);
+							//		        if (!world.isRemote)
+							//		        {
+							//		            world.markBlockForUpdate(pos);
+							//		        }
+						}
+			}
+			else if (clientSentence.startsWith(PLAYERGETPOS)) {
+				entityGetPos(player);
+			}
+			else if (clientSentence.startsWith(PLAYERGETTILE)) {
+				entityGetTile(player);
+			}
+			else if (clientSentence.startsWith(CHAT)) {
+				player.sendChatMessage(clientSentence.substring(args));
+			}
+			else if (clientSentence.startsWith(GETPLAYERIDS)) {
+				sendLine(""+player.getEntityId());
+			}
+			else if (clientSentence.startsWith(GETPLAYERENTITYID)) {
+				sendLine(""+player.getEntityId());
+			}
+			else if (clientSentence.startsWith(PLAYERSETTILE)) {
+				entitySetTile(player, scan);
+			}
+			else if (clientSentence.startsWith(PLAYERSETPOS)) {
+				entitySetPos(player, scan);
+			}
+			else if (clientSentence.startsWith(PLAYERGETDIRECTION)) {
+				entityGetDirection(player);
+			}
+			else if (clientSentence.startsWith(PLAYERGETROTATION)) {
+				entityGetRotation(player);
+			}
+			else if (clientSentence.startsWith(PLAYERGETPITCH)) {
+				entityGetPitch(player);
+			}
+			else if (clientSentence.startsWith(ENTITYGETPOS)) {
+				Entity e = world.getEntityByID(scan.nextInt());
+				if (e != null)
+					entityGetPos(e);
+				else
+					sendLine("Error: No such entity");
+			}
+			else if (clientSentence.startsWith(ENTITYGETTILE)) {
+				Entity e = world.getEntityByID(scan.nextInt());
+				if (e != null)
+					entityGetTile(e);
+				else
+					sendLine("Error: No such entity");
+			}
+			else if (clientSentence.startsWith(ENTITYGETROTATION)) {
+				Entity e = world.getEntityByID(scan.nextInt());
+				if (e != null)
+					entityGetRotation(e);
+				else
+					sendLine("Error: No such entity");
+			}
+			else if (clientSentence.startsWith(ENTITYGETPITCH)) {
+				Entity e = world.getEntityByID(scan.nextInt());
+				if (e != null)
+					entityGetPitch(e);
+				else
+					sendLine("Error: No such entity");
+			}
+			else if (clientSentence.startsWith(ENTITYGETDIRECTION)) {
+				Entity e = world.getEntityByID(scan.nextInt());
+				if (e != null)
+					entityGetDirection(e);
+				else
+					sendLine("Error: No such entity");
+			}
+			else if (clientSentence.startsWith(ENTITYSETTILE)) {
+				Entity e = world.getEntityByID(scan.nextInt());
+				if (e != null)
+					entitySetTile(e, scan);
+				
+			}
+			else if (clientSentence.startsWith(ENTITYSETPOS)) {
+				Entity e = world.getEntityByID(scan.nextInt());
+				if (e != null)
+					entitySetPos(e, scan);
+			}
+			scan.close();
+			scan = null;
+		}
+		catch(InputMismatchException e) {
+			System.out.println(""+e);
+		}
+		catch (NoSuchElementException e) {
+			System.out.println(""+e);
+		}
+		finally {
+			if (scan != null)
+				scan.close();
+		}
+	}
+
+
+	private void entityGetPitch(Entity e) {
+		Vec3 look = e.getLookVec();
+		Double xz = Math.sqrt(look.xCoord * look.xCoord + look.zCoord * look.zCoord);
+		sendLine(Math.atan2(look.yCoord, xz) * 180. / Math.PI);
+	}
+
+	private void entityGetRotation(Entity e) {
+		Vec3 look = e.getLookVec();
+		if (Math.abs(look.xCoord) < 1e-10 && Math.abs(look.yCoord) < 1e-10) {
+			sendLine(0);
+		}
+		else {
+			Double angle = (Math.atan2(look.zCoord, look.xCoord) + Math.PI) * 180. / Math.PI;
+			sendLine(angle);
+		}
+	}
+
+	private void entityGetDirection(Entity e) {
+		sendLine(e.getLookVec());
+	}
+
+	private void entitySetPos(Entity e, Scanner scan) {
+		double x = scan.nextDouble();
+		double y = scan.nextDouble();
+		double z = scan.nextDouble();
+		BlockPos spawnPos = world.getSpawnPoint();
+		e.setPositionAndUpdate(x + spawnPos.getX(), 
+				y + spawnPos.getY(),
+				z + spawnPos.getZ());
+	}
+
+	private void entitySetTile(Entity e, Scanner scan) {
+		BlockPos pos = getPosition(scan);
+		e.setPositionAndUpdate((double)pos.getX(), (double)pos.getY(), (double)pos.getZ());
+	}
+
+	private void entityGetTile(Entity e) {
+		BlockPos spawn = world.getSpawnPoint();
+		Vec3i spawnPoint = new Vec3i(spawn.getX(), spawn.getY(), spawn.getZ());
+		BlockPos pos = e.getPosition().subtract(spawnPoint);
+
+		sendLine(pos);
+	}
+
+	private void sendLine(BlockPos pos) {
+		sendLine(""+pos.getX()+","+pos.getY()+","+pos.getZ());
+	}
+
+	private void entityGetPos(Entity e) {
+		BlockPos spawn = world.getSpawnPoint();
+		Vec3 spawnPoint = new Vec3(spawn.getX(), spawn.getY(), spawn.getZ());
+		Vec3 pos = e.getPositionVector().subtract(spawnPoint);
+		sendLine(pos);
+	}
+
+	private void sendLine(double x) {
+		sendLine(Double.toString(x));
+	}
+
+	private void sendLine(int x) {
+		sendLine(Integer.toString(x));
+	}
+
+	private void sendLine(Vec3 v) {
+		sendLine(""+v.xCoord+","+v.yCoord+","+v.zCoord);
+	}
+
+	private void sendLine(String string) {
+		try {
+			writer.writeBytes(string+"\n");
+			writer.flush();
+		}
+		catch(Exception e) {					
+		}
+	}
+
+	private BlockPos getPosition(Scanner scan) {
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		int z = scan.nextInt();
+		return getPosition(x, y, z);
+	}
+
+	private BlockPos getPosition(int x, int y, int z) {
+		return world.getSpawnPoint().add(x,y,z);
+	}
+
+	public void close() {
+		try {
+			if (socket != null)
+				socket.close();
+		} catch (IOException e) {
+		}
+	}
+
+	private void initTypeMap() {
+		typeMap = new Block[256];
+		typeMap[0] = Blocks.air;
+		typeMap[1] = Blocks.stone;
+		typeMap[2] = Blocks.grass;
+		typeMap[3] = Blocks.dirt;
+		typeMap[4] = Blocks.cobblestone;
+		typeMap[5] = Blocks.planks; // wooden plank
+		typeMap[6] = Blocks.sapling;
+		typeMap[7] = Blocks.bedrock;
+		typeMap[8] = Blocks.flowing_water;
+		typeMap[9] = Blocks.water;
+		typeMap[10] = Blocks.flowing_lava;
+		typeMap[11] = Blocks.lava;
+		typeMap[12] = Blocks.sand;
+		typeMap[13] = Blocks.gravel;
+		typeMap[14] = Blocks.gold_ore;
+		typeMap[15] = Blocks.iron_ore;
+		typeMap[16] = Blocks.coal_ore;
+		typeMap[17] = Blocks.log; // wood
+		typeMap[18] = Blocks.leaves;
+		typeMap[19] = Blocks.sponge;
+		typeMap[20] = Blocks.glass;
+		typeMap[21] = Blocks.lapis_ore;
+		typeMap[22] = Blocks.lapis_block;
+		typeMap[24] = Blocks.sandstone;
+		typeMap[26] = Blocks.bed;
+		typeMap[27] = Blocks.redstone_block; // powered_rail
+		typeMap[30] = Blocks.web;
+		typeMap[31] = Blocks.tallgrass;
+		typeMap[32] = Blocks.deadbush;
+		typeMap[35] = Blocks.wool;
+		typeMap[37] = Blocks.yellow_flower; 
+		typeMap[38] = Blocks.red_flower;
+		typeMap[39] = Blocks.brown_mushroom;
+		typeMap[40] = Blocks.red_mushroom;
+		typeMap[41] = Blocks.gold_block;
+		typeMap[42] = Blocks.iron_block;
+		typeMap[43] = Blocks.stone_slab2;
+		typeMap[44] = Blocks.stone_slab;
+		typeMap[45] = Blocks.brick_block;
+		typeMap[46] = Blocks.tnt;
+		typeMap[47] = Blocks.bookshelf;
+		typeMap[48] = Blocks.mossy_cobblestone;
+		typeMap[49] = Blocks.obsidian;
+		typeMap[50] = Blocks.torch;
+		typeMap[51] = Blocks.fire;
+		typeMap[52] = Blocks.mob_spawner;
+		typeMap[53] = Blocks.oak_stairs;
+		typeMap[54] = Blocks.chest;
+		typeMap[56] = Blocks.diamond_ore;
+		typeMap[57] = Blocks.diamond_block;
+		typeMap[58] = Blocks.crafting_table;
+		typeMap[59] = Blocks.wheat; // seeds
+		typeMap[60] = Blocks.farmland;
+		typeMap[61] = Blocks.furnace;
+		typeMap[62] = Blocks.lit_furnace; 
+		typeMap[63] = UNKNOWN_BLOCK; // sign_post
+		typeMap[64] = Blocks.oak_door;
+		typeMap[65] = Blocks.ladder;
+		typeMap[66] = Blocks.rail;
+		typeMap[67] = Blocks.stone_stairs; // cobblestone stairs
+		typeMap[68] = Blocks.wall_sign;
+		typeMap[71] = Blocks.iron_door;
+		typeMap[73] = Blocks.redstone_ore;
+		typeMap[74] = Blocks.lit_redstone_ore; // glowing redstone ore
+		typeMap[78] = Blocks.snow_layer;
+		typeMap[79] = Blocks.ice;
+		typeMap[80] = Blocks.snow;
+		typeMap[81] = Blocks.cactus;
+		typeMap[82] = Blocks.clay;
+		typeMap[83] = UNKNOWN_BLOCK; // sugar cane
+		typeMap[85] = Blocks.oak_fence;
+		typeMap[86] = Blocks.pumpkin;
+		typeMap[87] = Blocks.netherrack;
+		typeMap[89] = Blocks.glowstone;
+		typeMap[91] = Blocks.lit_pumpkin;
+		typeMap[92] = Blocks.cake;
+		typeMap[95] = UNKNOWN_BLOCK; // invisible bedrock
+		typeMap[96] = Blocks.trapdoor;
+		typeMap[98] = Blocks.stonebrick;
+		typeMap[99] = Blocks.brown_mushroom; // huge brown mushroom
+		typeMap[100] = Blocks.red_mushroom; // huge red mushroom
+		typeMap[101] = Blocks.iron_bars;
+		typeMap[102] = Blocks.glass_pane;
+		typeMap[103] = Blocks.melon_block;
+		typeMap[104] = Blocks.pumpkin_stem;
+		typeMap[105] = Blocks.melon_stem;
+		typeMap[106] = Blocks.vine;
+		typeMap[107] = Blocks.oak_fence_gate;
+		typeMap[108] = Blocks.brick_stairs;
+		typeMap[109] = Blocks.stone_brick_stairs;
+		typeMap[110] = Blocks.mycelium;
+		typeMap[111] = Blocks.waterlily;
+		typeMap[112] = Blocks.nether_brick;
+		typeMap[114] = Blocks.nether_brick_stairs;
+		typeMap[120] = Blocks.end_portal_frame;
+		typeMap[121] = Blocks.end_stone;
+		typeMap[127] = Blocks.cocoa;
+		typeMap[128] = Blocks.sandstone_stairs;
+		typeMap[129] = Blocks.emerald_ore;
+		typeMap[133] = Blocks.emerald_block;
+		typeMap[134] = Blocks.spruce_stairs;
+		typeMap[135] = Blocks.birch_stairs;
+		typeMap[136] = Blocks.jungle_stairs;
+		typeMap[139] = Blocks.cobblestone_wall;
+		typeMap[141] = Blocks.carrots;
+		typeMap[142] = Blocks.potatoes;
+		typeMap[155] = Blocks.quartz_block;
+		typeMap[156] = Blocks.quartz_stairs;
+		typeMap[157] = UNKNOWN_BLOCK; // double wooden slab
+		typeMap[158] = Blocks.wooden_slab;
+		typeMap[159] = Blocks.stained_hardened_clay;
+		typeMap[163] = Blocks.acacia_stairs;
+		typeMap[164] = Blocks.dark_oak_stairs;
+		typeMap[170] = Blocks.hay_block;
+		typeMap[171] = Blocks.carpet;
+		typeMap[172] = Blocks.hardened_clay;
+		typeMap[173] = Blocks.coal_block;
+		typeMap[174] = Blocks.packed_ice;
+		typeMap[243] = UNKNOWN_BLOCK; // podzol
+		typeMap[244] = UNKNOWN_BLOCK; // beet root
+		typeMap[245] = UNKNOWN_BLOCK; // stone cutter
+		typeMap[246] = Blocks.obsidian; // glowing obsidian
+		typeMap[247] = UNKNOWN_BLOCK; // nether reactor core
+		typeMap[248] = UNKNOWN_BLOCK; // update game block
+		typeMap[249] = UNKNOWN_BLOCK; // update game block		
+	}
+
+	public Block getBlockByRaspberryType(int type) {
+		Block b = null;
+		if (type < typeMap.length)
+			b = typeMap[type];
+
+		if (b == null)
+			return UNKNOWN_BLOCK;
+		else
+			return b;
+	}
+
+	public int getRaspberryType(Block b) {
+		for (int i=0; i<typeMap.length; i++) 
+			if (typeMap[i] == b)
+				return i;
+		return 157; // UNKNOWN : double wood slab
+	}
+}
---
+++ src/main/java/mobi/omegacentauri/raspberryjammod/RaspberryJamMod.java
@@ -0,0 +1,59 @@
+package mobi.omegacentauri.raspberryjammod;
+
+import ibxm.Player;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import net.minecraft.block.state.BlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.init.Blocks;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = RaspberryJamMod.MODID, version = RaspberryJamMod.VERSION, name = RaspberryJamMod.NAME)
+public class RaspberryJamMod
+{
+	public static final String MODID = "raspberryjammod";
+	public static final String VERSION = "0.0";
+	public static final String NAME = "Raspberry Jam Mod";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		// some example code
+		System.out.println("Raspberry Jam Mod started");
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				MinecraftCommunicator mcc = null;
+				try {
+					mcc = new MinecraftCommunicator();
+					mcc.communicate();
+				} catch(IOException e) {
+					System.out.println("RaspberryJamMod error "+e);
+				}
+				finally {
+					if (mcc != null)
+						mcc.close();
+				}
+			}
+
+		}).start();
+	}
+
+}
