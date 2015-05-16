c0a99b15b46ae20c58148afba1fa2c0742842a64
arpruss <arpruss@gmail.com>
2015-05-15 20:17:56 -0500

    foo

---
+++ python2-scripts/mcpipy/simplefractaltree.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+def tree(depth):
+    t.up(30)
+    t.go(20)
+    if depth > 1:
+       tree(depth-1)
+    t.back(20)
+    t.down(60)
+    t.go(20)
+    if depth > 1:
+       tree(depth-1)
+    t.back(20)
+    t.up(30)
+
+t = Turtle()
+t.nofollow()
+t.verticalangle(90)
+t.pendelay(0)
+t.go(20)
+tree(5)
