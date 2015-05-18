5d9a6d9c598c16b98d56575564a9b7e47682ada9
arpruss <arpruss@gmail.com>
2015-05-18 09:53:50 -0500

    add turtle donut script

---
+++ python3-scripts/mcpipy/turtledonut.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+t = Turtle()
+t.pendelay(0)
+radius = 20
+circumference = 2 * radius * pi
+t.penwidth(10)
+t.penblock(GLASS)
+for i in range(90):
+    t.go(circumference/90)
+    t.left(4)
+t.penwidth(7)
+t.penblock(GOLD_BLOCK)
+for i in range(90):
+    t.go(circumference/90)
+    t.left(4)
+t.penup()
+t.back(20)
