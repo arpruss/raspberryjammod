c1c3fa9cda720678da5f72ecbf6c4567bf441b4a
arpruss <arpruss@gmail.com>
2015-05-25 13:01:32 -0500

    space fill

---
+++ python3-scripts/mcpipy/spacefill.py
@@ -0,0 +1,26 @@
+import lsystem
+from turtle import *
+t = Turtle()
+t.pendelay(0)
+t.turtle(None)
+t.penblock(STAINED_GLASS_PURPLE)
+t.gridalign()
+
+# rules from
+# http://kanga.nu/~claw/blog/2008/11/16/game-design-tools/inkscape-l-systems-svg-penrose-and-other-tilings/
+rules = { 'W': '+++X--F--ZFX+',
+          'X': '---W++F++YFW-',
+          'Y': '+ZFX--F--Z+++',
+          'Z': '-YFW++F++Y---',
+          }
+axiom = 'W'
+
+dictionary = {
+  'F': lambda: t.go(6),
+  '+': lambda: t.yaw(-30),
+  '-': lambda: t.yaw(30),
+  '[': lambda: t.push(),
+  ']': lambda: t.pop()
+  }
+
+lsystem.lsystem(axiom, rules, dictionary, 6)
