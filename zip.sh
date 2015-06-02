2ac9100557ee6d0899d4bb36d5ee76df4469fcfb
arpruss <arpruss@gmail.com>
2015-06-01 21:33:48 -0500

    foo

--- zip.sh
+++ zip.sh
@@ -7,7 +7,9 @@ chmod -R u+rw python[23]-scripts
 #done
 #rm -f *.{bak,pyc} */*.{bak,pyc}
 #cd ..
-cd python3-scripts
+cd python3-scripts/mcpipy
+rm -rf *.{bak,pyc} */*.{bak,pyc} __pycache__ */__pycache__ */*/__pycache__
+cd ..
 zip -9r ../python3-scripts.zip mcpipy
 cd ../python2-scripts
 zip -9r ../python2-scripts.zip mcpipy
