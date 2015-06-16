844622c60ec235688e36a61d44d486c833a20365
arpruss <arpruss@gmail.com>
2015-06-15 20:34:38 -0500

    coloring

--- python2-scripts/mcpipy/mandelbulb.py
+++ python2-scripts/mcpipy/mandelbulb.py
@@ -13,25 +13,30 @@ try:
 except:
    cpuCount = 1
 
-ESCAPE = 30
+ESCAPE = 250
 if len(sys.argv) < 2:
     SIZE = 100
 else:
     SIZE = int(sys.argv[1])
-FRACTALSIZE = 2.25
+FRACTALSIZE = 2.2
+
+palette = list(reversed([WOOL_WHITE,HARDENED_CLAY_STAINED_WHITE,WOOL_PINK,WOOL_LIGHT_GRAY,WOOL_LIGHT_BLUE,WOOL_MAGENTA,WOOL_PURPLE,HARDENED_CLAY_STAINED_LIGHT_BLUE,HARDENED_CLAY_STAINED_LIGHT_GRAY,HARDENED_CLAY_STAINED_MAGENTA,HARDENED_CLAY_STAINED_PINK,HARDENED_CLAY_STAINED_RED,WOOL_RED,REDSTONE_BLOCK,HARDENED_CLAY_STAINED_ORANGE,WOOL_ORANGE,HARDENED_CLAY_STAINED_YELLOW,WOOL_YELLOW,WOOL_LIME,HARDENED_CLAY_STAINED_LIME,HARDENED_CLAY_STAINED_PURPLE,HARDENED_CLAY_STAINED_CYAN,WOOL_CYAN,WOOL_BLUE,HARDENED_CLAY_STAINED_BLUE,WOOL_GRAY,HARDENED_CLAY_STAINED_GREEN,WOOL_GREEN,HARDENED_CLAY_STAINED_BROWN,WOOL_BROWN,HARDENED_CLAY_STAINED_GRAY,WOOL_BLACK]));
 
 block = WOOL_WHITE # REDSTONE_ORE
 
-def escapes(pos):
+def calculate(pos):
     x,z,y = pos[0],pos[1],pos[2]
     cx,cy,cz = x,y,z
 
     i = 0
     try:
+        wentOut = 0
         while i<ESCAPE:
             r = sqrt(x*x+y*y+z*z)
-            if r > 31:
-               return True
+            if r > 2:
+               return -1
+            if r > wentOut:
+               wentOut = r
             theta = acos(z/r)
             phi = atan2(y,x)
             zr = r**8
@@ -43,9 +48,9 @@ def escapes(pos):
             z = cz+zr*cos(theta)
 
             i = i + 1
-        return False
+        return wentOut
     except:
-        return False
+        return 0
 
 #
 # we could of course just do for x in range(0,size): for y in range(0,size): yield(x,y)
@@ -80,10 +85,14 @@ def drawCore(core,cores,centerBulb,scale,cornerMC,size,block):
 #        minecraft.setBlocks(mcX,cornerMC.y,cornerMC.z,mcX,cornerMC.y+size-1,cornerMC.z+size-1,AIR)
         for mcY in range(cornerMC.y, cornerMC.y+size):
             for mcZ in range(cornerMC.z, cornerMC.z+size):
-                if not escapes(toBulb(centerMC,centerBulb,scale,mcX,mcY,mcZ)):
-                    minecraft.setBlock(mcX,mcY,mcZ,block)
+                radius = calculate(toBulb(centerMC,centerBulb,scale,mcX,mcY,mcZ))
+                if radius < 0:
+                    minecraft.setBlock(mcX,mcY,mcZ,AIR)
                 else:
-                    minecraft.setBlock(mcX,mcY,mcZ,AIR) 
+                    i = int(len(palette) / 1.08 * radius)
+                    if i >= len(palette):
+                        i = len(palette) - 1
+                    minecraft.setBlock(mcX,mcY,mcZ,palette[i])
         if core==0 and pollZoom():
             return
     minecraft.postToChat("Core "+str(core+1)+" of "+str(cores)+" has rendered")
