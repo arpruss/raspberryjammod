d8687bb38af4fe97fe72fc37bcef547f5be56ae1
arpruss <arpruss@gmail.com>
2015-06-18 09:04:07 -0500

    fix connection.py ; improve text module

--- python2-scripts/mcpipy/digitalclock.py
+++ python2-scripts/mcpipy/digitalclock.py
@@ -1,35 +1,39 @@
 from mc import *
-import text8x8
+import text
 import datetime
 import time
 import sys
+import fonts
 
 foreground = SEA_LANTERN
 background = AIR
 
 try:
-    if len(sys.argv) > 1:
-        foreground = int(sys.argv[1])
+    if len(sys.argv) > 1 and not "__" in sys.argv[1]:
+        foreground = eval(sys.argv[1])
 except:
     pass
 
 try:
-    if len(sys.argv) > 2:
-        background = int(sys.argv[2])
+    if len(sys.argv) > 2 and not "__" in sys.argv[2]:
+        background = eval(sys.argv[2])
 except:
     pass
 
 mc = Minecraft()
 pos = mc.player.getTilePos()
-forward = text8x8.angleToTextDirection(mc.player.getRotation())
+forward = text.angleToTextDirection(mc.player.getRotation())
 
 prevTime = ""
 
 while True:
     curTime = datetime.datetime.now().strftime("%I:%M:%S %p")
     if curTime[0]=='0':
-        curTime[0] = ' '
+        curTime = ' ' + curTime[1:]
     if prevTime != curTime:
-        text8x8.drawText8x8(mc, pos, forward, Vec3(0,1,0), curTime, foreground, background)
+        for i in range(len(curTime)):
+            if i >= len(prevTime) or prevTime[i] != curTime[i]:
+                text.drawText(mc, fonts.FONTS['8x8'], pos + forward * (8*i), forward, Vec3(0,1,0), curTime[i:], foreground, background)
+                break
         prevTime = curTime
     time.sleep(0.1)
