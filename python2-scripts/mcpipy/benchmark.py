87afe64e1b2307230c2c331d9c9270d6d4791069
arpruss <arpruss@gmail.com>
2015-06-30 08:50:09 -0500

    benchmark

---
+++ python2-scripts/mcpipy/benchmark.py
@@ -0,0 +1,17 @@
+from time import time
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+t = time()
+for i in range(1000):
+    mc.getBlock(0,0,0)
+print "getBlock : {}ms".format(1000.*(time()-t)/1000.)
+t = time()
+for i in range(10000):
+    mc.setBlock(0,0,0,1)
+mc.getBlock(0,0,0)
+print "setBlock same : {}ms".format(1000.*(time()-t)/10000.)
+t = time()
+for i in range(10000):
+    mc.setBlock(0,0,0,i&1)
+mc.getBlock(0,0,0)
+print "setBlock different : {}ms".format(1000.*(time()-t)/10000.)
--- python2-scripts/mcpipy/mcpi/settings.py
+++ python2-scripts/mcpipy/mcpi/settings.py
@@ -1,1 +1,6 @@
+POCKET_EDITION = 0
+PI = 1
+DESKTOP = 2
+
+minecraftType = DESKTOP
 isPE = False
