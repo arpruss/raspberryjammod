556482ba04808207a6a65d04a8be6c92d792f69b
arpruss <arpruss@gmail.com>
2015-07-13 23:16:49 -0500

    foo

--- python2-scripts/mcpipy/render.py
+++ python2-scripts/mcpipy/render.py
@@ -28,6 +28,7 @@ import sys
 import mcpi.minecraft as minecraft
 #import minecraft block module
 import mcpi.block as block
+from mcpi.block import *
 #import time, so delays can be used
 import time
 #import datetime, to get the time!
@@ -38,29 +39,48 @@ import math
 import os
 import re
 
-class ObjDescriptor(object):
-    def __init__(self,in):
+def safeEval(p):
+    if '__' in p:
+        raise ValueError("Insecure entry")
+    return eval(p)
+
+def parseBlock(data):
+    b = Block(0,0)
+    tokens = re.split("[\\s,]+", data)
+    haveBlock = False
+    start = safeEval(tokens[0])
+    if isinstance(start,Block):
+        b = start
+    else:
+        b.id = int(start)
+    if len(tokens)>1:
+        b.data = int(safeEval(tokens[1]))
+    return (b.id,b.data)
+
+class Mesh(object):
+    def __init__(self,infile):
          self.swapYZ = False
          self.size = 100
          self.default = (BEDROCK.id, 0)
-         self.materialNames = set()
-         self.materialDict = {}
+         self.materialBlockDict = {}
          self.baseVertices = []
          self.vertices = []
          self.textures = []
          self.normals = []
          self.faces = []
          self.materials = []
+         self.materialNames = []
          self.corner1 = None
          self.corner2 = None
 
-         base,ext = os.path.splitext(in)
+         base,ext = os.path.splitext(infile)
          if ext == '.obj':
-             self.objName = in
+             self.objName = infile
              self.dataFile = base + ".txt"
-         with open(in) as f:
-             self.dataFile = base + ".txt"
+         with open(infile) as f:
+             self.dataFile = infile
              self.objName = base + ".obj"
+             dirname = os.path.dirname(infile)
              materialMode = False
              for line in f:
                  line = line.strip()
@@ -70,12 +90,12 @@ class ObjDescriptor(object):
                  if found:
                      token = found.group(1).lower()
                      if materialMode:
-                         self.materialDict[found.group(1)] = parseBlock(found.group(2))
+                         self.materialBlockDict[found.group(1)] = parseBlock(found.group(2))
                      elif token == "file":
                          if found.group(2).startswith('"') or found.group(2).startswith("'"):
-                             self.objName = safeEval(found.group(2))
+                             self.objName = dirname + "/" + safeEval(found.group(2))
                          else:
-                             self.objName = found.group(2)
+                             self.objName = dirname + "/" + found.group(2)
                      elif token == "swapyz":
                          self.swapYZ = bool(safeEval(found.group(2).capitalize()))
                      elif token == "size":
@@ -86,17 +106,19 @@ class ObjDescriptor(object):
                      materialMode = True
 
     def read(self):
-        if swapYZ:
+        if self.swapYZ:
             fix = lambda list : V3(list[0], list[2], list[1])
         else:
             fix = lambda list : V3(list)
 
-        currentMaterial = "(unspecified)"
+        currentMaterialIndex = 0
+        self.materialBlocks = [ self.default ]
+        self.materialIndexDict = { "(unspecified)": 0 }
 
-        with open(filename) as fh:
+        with open(self.objName) as fh:
             for line in fh :
                 line = line.strip()
-                if line[0] == '#' : continue
+                if len(line) == 0 or line[0] == '#' : continue
                 line = re.split('\s+', line)
                 if line[0] == 'v' :
                     self.baseVertices.append(V3(fix([float(x) for x in line[1:]])))
@@ -115,44 +137,63 @@ class ObjDescriptor(object):
                                 face[i][j] = int(face[i][j]) - 1
                     #append the material currently in use to the face
                     self.faces.append(face)
-                    self.materials.append(currentMaterial)
+                    self.materials.append(currentMaterialIndex)
                 elif line[0] == 'usemtl': # material
-                    currentMaterial = line[1]
-                    materialNames.add(currentMaterial)
+                    name = line[1]
+                    try:
+                        currentMaterialIndex = self.materialIndexDict[name]
+                    except KeyError:
+                        currentMaterialIndex = len(self.materialBlocks)
+                        self.materialIndexDict[name] = currentMaterialIndex
+                        self.materialBlocks.append(self.materialBlockDict.get(name, self.default))
 
-    def adjust(self, matrix, bottomCenter):
+    def scale(self, bottomCenter, matrix=None):
         minimum = [None, None, None]
         maximum = [None, None, None]
 
-        for i in range(len(baseVertices)):
+        self.vertices = []
+
+        for v in self.baseVertices:
             if matrix is not None:
-                self.vertices[i] = applyMatrix(matrix,self.baseVertices[i])
+                vertex = applyMatrix(matrix,v)
             else:
-                self.vertices[i] = self.baseVertices[i]
-            vertex = self.vertices[i]
+                vertex = v
+            self.vertices.append(v)
             for i in range(3):
                 if minimum[i] == None or vertex[i] < minimum[i]:
-                    minimum[i] = self.vertex[i]
+                    minimum[i] = vertex[i]
                 if maximum[i] == None or vertex[i] > maximum[i]:
-                    maximum[i] = self.vertex[i]
+                    maximum[i] = vertex[i]
 
         center = [(maximum[i] + minimum[i])/2 for i in range(3)]
 
-        maxsize = 0
+        maxsize = max( ( maximum[i]-minimum[i] for i in range(3) ) )
 
-        for i in range(3):
-            if maximum[i]-mininum[i] > maxsize:
-                maxsize = maxium[i]-mininum[i]
+        scale = self.size / maxsize
+        translate = V3(bottomCenter.x-scale*center[0], bottomCenter.y-scale*minimum[1], bottomCenter.z-scale*center[2])
 
-        scale = size / maxsize
-        translate = V3(bottomCenter.x-scale*center[0], bottomCenter.y-scale*mininum[1], bottomCenter.z-scale*center[2])
-
-        for i in range(len(baseVertices)):
+        for i in range(len(self.vertices)):
             self.vertices[i] = self.vertices[i] * scale + translate
 
         self.corner1 = (V3(minimum) * scale + translate).ifloor()
         self.corner2 = (V3(maximum) * scale + translate).iceil()
 
+    def drawVertices(self, mc, vertices, material):
+        block = self.materialBlocks[material]
+        for vertex in vertices:
+            if material != self.drawRecord.get(vertex):
+                mc.setBlock(vertex, block)
+                self.drawRecord[vertex] = material
+
+    def render(self, mc):
+        self.drawRecord = {}
+
+        for faceCount,face in enumerate(self.faces):
+            if faceCount % 5000 == 0:
+                mc.postToChat("{0:.1f}%".format(100. * faceCount / len(self.faces)))
+
+            faceVertices = [self.vertices[vertex[0]] for vertex in face]
+            self.drawVertices(mc, getFace(faceVertices), self.materials[faceCount])
 
 # class to create 3d filled polygons
 class MinecraftDrawing:
@@ -217,242 +258,23 @@ def load_obj(filename, swapyz, defaultBlock, materials) :
                 print "Warning: Couldn't find '" + str(usemtl) + "' in materials using default"
 
     return V, T, N, F, MF
+    
+def go(filename):
+    mc = minecraft.Minecraft()
+
+    mc.postToChat("Preparing")
+    mesh = Mesh(filename)
+    mc.postToChat("Reading")
+    mesh.read()
+    mc.postToChat("Scaling")
+    mesh.scale(mc.player.getPos())
+    mc.postToChat("Clearing")
+    mc.setBlocks(mesh.corner1,mesh.corner2,AIR)
+    mc.postToChat("Rendering")
+    mesh.render(mc)
+    mc.postToChat("Done!")
+
 
 # main program
 if __name__ == "__main__":
-
-    print datetime.datetime.now()
-
-    #Connect to minecraft by creating the minecraft object
-    # - minecraft needs to be running and in a game
-    mc = minecraft.Minecraft.create()
-
-    #Create minecraft drawing class
-    mcDrawing = MinecraftDrawing(mc)
-
-    player = mc.player.getPos()
-
-    WOOL_WHITE = (block.WOOL.id, 0)
-    WOOL_ORANGE = (block.WOOL.id, 1)
-    WOOL_MAGENTA = (block.WOOL.id, 2)
-    WOOL_LIGHT_BLUE = (block.WOOL.id, 3)
-    WOOL_YELLOW = (block.WOOL.id, 4)
-    WOOL_LIME = (block.WOOL.id, 5)
-    WOOL_PINK = (block.WOOL.id, 6)
-    WOOL_GRAY = (block.WOOL.id, 7)
-    WOOL_LIGHT_GRAY = (block.WOOL.id, 8)
-    WOOL_CYAN = (block.WOOL.id, 9)
-    WOOL_PURPLE = (block.WOOL.id, 10)
-    WOOL_BLUE = (block.WOOL.id, 11)
-    WOOL_BROWN = (block.WOOL.id, 12)
-    WOOL_GREEN = (block.WOOL.id, 13)
-    WOOL_RED = (block.WOOL.id, 14)
-    WOOL_BLACK = (block.WOOL.id, 15)
-
-    objects = {
-        'ds9': ('ds9.obj', False, 200, (block.STONE.id, None),
-                { "Yellow_self_illum": (block.GLOWSTONE_BLOCK.id, None),
-                      "Lamps": (block.GLOWSTONE_BLOCK.id, None),
-                      "Windows": (block.GLASS.id, None),
-                      "Phaser": (124,0),
-                      "Antenna": (173,0)
-                    }),
-        '1701d': ('1701d.obj', False, 200, (block.QUARTZ_BLOCK.id, None),
-                { "Yellow_self_illum": (block.GLOWSTONE_BLOCK.id, None),
-                      "Lamps": (block.SEA_LANTERN.id, None),
-                      "Windows": (block.GLASS.id, None),
-                      "Phaser": (block.REDSTONE_BLOCK.id,0),
-                      "Antenna": (173,0)
-                    }),
-        'shuttle': ('shuttle.obj', True, 100, (block.WOOL.id, 0),
-                    { "glass": (block.GLASS.id, None),
-                     "bone": (block.WOOL.id, 0),
-                     "fldkdkgrey": (block.WOOL.id, 7),
-                     "redbrick": (block.WOOL.id, 14),
-                     "black": (block.WOOL.id, 15),
-                     "brass": (block.WOOL.id, 1),
-                     "dkdkgrey": (block.WOOL.id, 7)
-                        }),
-        'skyscraper': ('skyscraper.obj', False, 100, (block.IRON_BLOCK.id, None), {}),
-        'head': ('head.obj', False, 50, (block.GOLD_BLOCK.id, None), {}),
-        'cessna': ('cessna.obj', False, 100, (block.IRON_BLOCK.id, None),
-               {
-                  "yellow": WOOL_YELLOW,
-                  "red": WOOL_RED,
-                  "white": WOOL_WHITE,
-                  "black": WOOL_BLACK,
-                  "glass": (block.GLASS.id, None),
-                  "dkgrey": WOOL_GRAY,
-               }),
-        'ny': ('NY_LIL.obj', False, 200, (block.IRON_BLOCK.id, None),
-               {
-                   "Default_Material": (block.WOOL.id, 0),
-                   "Color_A01": (block.WOOL.id, 14),
-                   "0131_Silver": (block.IRON_BLOCK.id, None),
-                   "0075_ForestGreen": (block.WOOL.id, 13),
-                   "0137_Black": (block.WOOL.id, 15),
-                   "Black": (block.WOOL.id, 15),
-                   "Medium_Brown": (block.WOOL.id, 12),
-                   "0056_Yellow": (block.WOOL.id, 4),
-                   "0020_Red": (block.WOOL.id, 14),
-                   "0102_RoyalBlue": (block.WOOL.id, 11),
-                   "Color_E01": (block.WOOL.id, 4),
-                   "Color_E02": (block.WOOL.id, 4),
-                   "Color_B01": (block.WOOL.id, 1),
-                   "Charcoal": (block.WOOL.id, 7),
-                   "Material2": (block.WOOL.id, 0),
-                   "Beige2": (block.SANDSTONE.id, None),
-                   "DarkGoldenrod": (block.GOLD_BLOCK.id, None),
-                   "Beige1": (block.SANDSTONE.id, None),
-                   "jean_blue": (block.WOOL.id, 3),
-                   "Gold1": (block.GOLD_BLOCK.id, None),
-                   "WhiteSmoke": (block.WOOL.id, 8),
-                   "0118_Thistle": (block.WOOL.id, 6),
-                   "Color_D23": (block.WOOL.id, 7),
-                   "Color_B23": (block.WOOL.id, 12),
-                   "Color_009": (block.WOOL.id, 15),
-                   "Color_D01": (block.WOOL.id, 1),
-                   "Color_A06": (block.WOOL.id, 14),
-                   "Color_D03": (block.WOOL.id, 4),
-                   "0063_GreenYellow": (block.WOOL.id, 5)
-                   }),
-        'nottingham': ('City_Ground-Notts.obj', False, 200, (block.DIRT.id, None),
-               {
-                    "Default_Material": (block.STONE.id, None),
-                    "Black": (block.WOOL.id, 15),
-                   "Asphalt_Old": (block.WOOL.id, 7),
-                   "GhostWhite": (block.WOOL.id, 0),
-                   "Brick_Flemish_Bond": (block.BRICK_BLOCK.id, None),
-                   "Concrete_Brushed": (block.STONE.id, None),
-                   "Metal_Brushed": (block.IRON_BLOCK.id, None),
-                   "Roofing_Metal_Standing_Seam_Blue": (block.WOOL.id, 8),
-                   "White": (block.WOOL.id, 0),
-                   "Metal_Brushed1": [(block.IRON_BLOCK.id, None),None],
-                   "Rouge3141": (block.WOOL.id, 14),
-                   "roof": (block.WOOL.id, 8),
-                   "Metal_Aluminum_Anodized": (block.IRON_BLOCK.id, None),
-                   "Translucent_Glass_Safety": (block.GLASS.id, None),
-                   "Translucent_Glass_Safety1": (block.GLASS.id, None),
-                   "Safety_Glass2": (block.GLASS.id, None),
-                   "Red": (block.WOOL.id, 14),
-                   "goal_net1": (block.WOOL.id, 0),
-                   "Black": (block.WOOL.id, 15)}),
-        'pi': ( 'RaspberryPi.obj', False, 150, (block.DIRT.id, None), 
-               {
-                   "Default_Material": (block.WOOL.id, 0),
-                   "Material1": (block.WOOL.id, 5),
-                   "Goldenrod": (block.WOOL.id, 1),
-                   "0136_Charcoal": (block.WOOL.id, 7),
-                   "Gray61": (block.WOOL.id, 7),
-                   "Charcoal": (block.WOOL.id, 7),
-                   "Color_002": (block.WOOL.id, 8),
-                   "Color_008": (block.WOOL.id, 4),
-                   "Plastic_Green": (block.WOOL.id, 5),
-                   "MB_Pastic_White": (block.WOOL.id, 0),
-                   "IO_Shiny": (block.IRON_BLOCK.id, None),
-                   "Material4": (block.GRASS.id, None),
-                   "Gainsboro3": (block.WOOL.id, 5),
-                   "CorrogateShiny1": (block.IRON_BLOCK.id, None),
-                   "Gold": (block.GOLD_BLOCK.id, None),
-                   "0129_WhiteSmoke": (block.WOOL.id, 0),
-                   "Color_005": (block.WOOL.id, 0),
-                   "USB_IO": (block.WOOL.id, 11),
-                   "_Metal": (block.IRON_BLOCK.id, None),
-                   "0132_LightGray": (block.WOOL.id, 8)})
-                   }
-
-    objectname = sys.argv[1] if len(sys.argv) >= 2 else 'pi'
-
-    if not objectname in objects:
-        print "Object not found"
-        exit(3)
-
-    filename,swapyz,size,defaultblock,materialdict = objects[objectname]
-
-    if len(sys.argv) >= 3:
-        size = float(sys.argv[2])
-        
-    yaw = 0
-
-    if len(sys.argv) >= 4:
-        yaw = float(sys.argv[3])
-           
-    pitch = 0
-
-    if len(sys.argv) >= 5:
-        pitch = float(sys.argv[4])
-
-    roll = 0
-
-    if len(sys.argv) >= 6:
-        roll = float(sys.argv[5])
-
-    matrix = None
-
-    if yaw != 0 or pitch != 0 or roll != 0:
-        matrix = makeMatrix(yaw, pitch, roll)
-
-    vertices,textures,normals,faces,materials = load_obj(filename, swapyz, defaultblock, materialdict)
-
-    min = [None, None, None]
-    max = [None, None, None]
-
-    for i in range(len(vertices)):
-        if matrix is not None:
-            vertices[i] = applyMatrix(matrix,vertices[i])
-        vertex = vertices[i]
-        for i in range(3):
-            if min[i] == None or vertex[i] < min[i]:
-                min[i] = vertex[i]
-            if max[i] == None or vertex[i] > max[i]:
-                max[i] = vertex[i]
-
-    maxsize = 0
-
-    center = [(max[i] + min[i])/2 for i in range(3)]
-    for i in range(3):
-        if max[i]-min[i] > maxsize:
-            maxsize = max[i]-min[i]
-
-    scale = size / maxsize
-
-    print "obj file loaded"
-
-    translate = (player.x-scale*center[0], player.y-scale*min[1], player.z-scale*center[2])
-
-    #Post a message to the minecraft chat window
-    mc.postToChat("Hi, Minecraft 3d model maker, www.stuffaboutcode.com")
-
-    # clear a suitably large area
-    mc.setBlocks(int(translate[0]+scale*min[0]),
-                 int(translate[1]+scale*min[1]),
-                 int(translate[2]+scale*min[2]),
-                 int(math.ceil(translate[0]+scale*max[0])),
-                 int(math.ceil(translate[1]+scale*max[1])),
-                 int(math.ceil(translate[2]+scale*max[2])),
-                 block.AIR)
-
-    scaledVertices = []
-    for vertex in vertices:
-        scaledVertices.append(vertex * scale + translate)
-
-    faceCount = 0
-    # loop through faces
-
-    drawRecord = {}
-
-    print len(faces),"faces"
-
-    for face in faces:
-        if faceCount % 10000 == 0:
-             print faceCount
-
-        faceVertices = [scaledVertices[vertex[0]] for vertex in face]
-
-        # draw the face
-        mcDrawing.drawFace(drawRecord, faceVertices, materials[faceCount])
-        faceCount += 1
-
-    mc.postToChat("Model complete, www.stuffaboutcode.com")
-
-    print datetime.datetime.now()
+    go("models/" + sys.argv[1] + ".txt")
