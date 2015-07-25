f9f1a9aa03a0ddc52324fbb6465b931d3ff1db39
arpruss <arpruss@gmail.com>
2015-07-24 23:18:00 -0500

    foo

--- python2-scripts/mcpipy/mcpi/settings.py
+++ python2-scripts/mcpipy/mcpi/settings.py
@@ -1,6 +1,15 @@
+from os import environ
+
 MINECRAFT_POCKET_EDITION = 0
 MINECRAFT_PI = 1
 MINECRAFT_DESKTOP = 2
 
 minecraftType = MINECRAFT_DESKTOP
-isPE = False
+
+try:
+     minecraftType = int(environ['MINECRAFT_TYPE'])
+except:
+     pass
+
+isPE = ( minecraftType != MINECRAFT_DESKTOP )
+
