5d96f6f397dc7cfa7c86b441616137e614aba21a
BuildTools <arpruss@gmail.com>
2015-09-08 07:36:41 -0500

    dither

--- python2-scripts/mcpipy/camera.py
+++ python2-scripts/mcpipy/camera.py
@@ -1,7 +1,7 @@
 from mc import *
-import pygame
 import pygame.camera
 from time import sleep
+from sys import argv
 
 COLORS = ( (35,0, 222,222,222),
 		(35,1, 219,125,63),
@@ -43,29 +43,58 @@ COLORS = ( (35,0, 222,222,222),
 def colorDist(a,b):
     return (a[0]-b[0])*(a[0]-b[0])+(a[1]-b[1])*(a[1]-b[1])+(a[2]-b[2])*(a[2]-b[2])
 
-def rgbToBlock(rgb):
-    bestColor = COLORS[0][0:2]
+def getBestColor(rgb):
+    bestColor = COLORS[0]
     bestDist = 255*255*3
     for c in COLORS:
         d = colorDist(c[2:],rgb)
         if d < bestDist:
             bestDist = d
-            bestColor = c[0:2]
+            bestColor = c
     return bestColor
 
 mc = Minecraft()
 pos = mc.player.getTilePos()
 
-pygame.init()
+dither = True
+width = 80
+if len(argv) >= 2:
+   width = int(argv[1])
+height = width * 3 // 4
+
 pygame.camera.init()
 camlist = pygame.camera.list_cameras()
 if camlist:
    cam = pygame.camera.Camera(camlist[0],(640,480))
+   current = [[(-1,-1) for y in range(height)] for x in range(width)]
    while True:
-       image = pygame.transform.scale(cam.get_image(), (120,90))
-       for x in range(120):
-           for y in range(90):
-               block = rgbToBlock(image.get_at((x,89-y))[0:3])
-               mc.setBlock(pos.x+x,pos.y+y,pos.z,block)
+       image = pygame.transform.scale(cam.get_image(), (width,height))
+       if not dither:
+           for x in range(width):
+               for y in range(height):
+                   block = getBestColor(image.get_at((x,y)))[0:2]
+                   if current[x][y] != block:
+                       mc.setBlock(pos.x+x,pos.y+height-1-y,pos.z,block)
+                       current[x][y] = block
+       else:
+           pixels = [[list(image.get_at((x,y))[0:3]) for y in range(height)] for x in range(width)]
+           for x in range(width):
+               for y in range(height):
+                   color = getBestColor(pixels[x][y])
+                   block = color[0:2]
+                   if current[x][y] != block:
+                       mc.setBlock(pos.x+x,pos.y+height-1-y,pos.z,block)
+                       current[x][y] = block
+                   for i in range(3):
+                       err = pixels[x][y][i] - color[2+i]
+                       if x + 1 < width:
+                           pixels[x+1][y][i] += err * 7 // 16
+                       if y + 1 < height:
+                           if 0 < x:
+                               pixels[x-1][y+1][i] += err * 3 // 16
+                           pixels[x][y+1][i] += err * 5 // 16
+                           if x + 1 < width:
+                               pixels[x+1][y+1][i] += err // 16
+
 else:
    mc.postToChat('Camera not found')
