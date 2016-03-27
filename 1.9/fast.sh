3a15ab64087500580ff76c744833291b11c7666d
BuildTools <arpruss@gmail.com>
2016-03-26 19:53:09 -0500

    foo

--- 1.9/fast.sh
+++
@@ -1,6 +0,0 @@
-rm build/libs/Raspberr*.jar
-./gradlew.bat --offline build
-rm build/libs/Raspberry*ources.jar
-mv build/libs/Raspberr* build/libs/RaspberryJamMod.jar
-cp build/libs/RaspberryJamMod.jar $APPDATA/.minecraft/mods/1.9/
-
