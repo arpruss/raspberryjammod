3850e206327ecc62d75babc7e7009373b5c14dab
arpruss <arpruss@gmail.com>
2016-08-08 11:39:36 -0500

    mcpipy

--- zip.sh
+++ zip.sh
@@ -1,10 +1,10 @@
 rm python-scripts.zip
-echo AUTHENTICATION_USERNAME=None > python-scripts/mcpipy/mcpi/security-template.py
-echo AUTHENTICATION_PASSWORD=None >> python-scripts/mcpipy/mcpi/security-template.py
-cd python-scripts/mcpipy
+echo AUTHENTICATION_USERNAME=None > mcpipy/mcpi/security-template.py
+echo AUTHENTICATION_PASSWORD=None >> mcpipy/mcpi/security-template.py
+cd mcpipy
 rm -rf *.{bak,pyc} */*.{bak,pyc} __pycache__ */__pycache__ */*/__pycache__
 rm -f security.py
 rm -f _sunfish.*
 cd ..
-zip -9r ../python-scripts.zip mcpipy
-cd ..
+zip -9r python-scripts.zip mcpipy
+
