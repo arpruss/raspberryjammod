304ef2a9a263c1e2218394f9131c60bba7cad4b5
arpruss <arpruss@gmail.com>
2016-08-08 11:35:41 -0500

    dual

--- zip.sh
+++ zip.sh
@@ -1,17 +1,10 @@
-rm python2-scripts.zip
-rm python3-scripts.zip
-echo AUTHENTICATION_USERNAME=None > python2-scripts/mcpipy/mcpi/security.py
-echo AUTHENTICATION_PASSWORD=None >> python2-scripts/mcpipy/mcpi/security.py
-chmod -R u+rw python[23]-scripts
-rm -r python3-scripts
-cp -r python2-scripts python3-scripts
-2to3 -w -f all -x imports -x xrange -x range -x future python3-scripts
-cd python3-scripts/mcpipy
+rm python-scripts.zip
+echo AUTHENTICATION_USERNAME=None > python-scripts/mcpipy/mcpi/security-template.py
+echo AUTHENTICATION_PASSWORD=None >> python-scripts/mcpipy/mcpi/security-template.py
+cd python-scripts/mcpipy
 rm -rf *.{bak,pyc} */*.{bak,pyc} __pycache__ */__pycache__ */*/__pycache__
+rm -f security.py
+rm -f _sunfish.*
 cd ..
-zip -9r ../python3-scripts.zip mcpipy
-cd ../python2-scripts
-rm mcpipy/*.pyc mcpipy/*/*.pyc mcpipy/_sunfish.*
-zip -9r ../python2-scripts.zip mcpipy
-rm mcpipy/*.pyc mcpipy/*/*.pyc mcpipy/_sunfish.*
+zip -9r ../python-scripts.zip mcpipy
 cd ..
