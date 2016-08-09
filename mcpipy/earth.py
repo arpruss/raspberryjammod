ec1f77943b783987085155ac5c7d1facb2b7bc94
arpruss <arpruss@gmail.com>
2016-08-09 10:48:47 -0500

    earth

---
+++ mcpipy/earth.py
@@ -0,0 +1,36 @@
+#
+# Code by Alexander Pruss and under the MIT license
+#
+# Needs Pillow
+#
+
+#
+# earth.py [size [filename]]
+#
+
+from mc import *
+import os.path,sys
+from PIL import Image
+from pysanka import egg, getPixel
+
+mc = Minecraft()
+pos = mc.player.getTilePos()
+
+height = 100
+filename = None
+
+if len(sys.argv) > 1:
+    height = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        filename = sys.argv[1]
+        if not os.path.isfile(filename):
+            filename = os.path.dirname(os.path.realpath(sys.argv[0])) + "/" + filename    
+
+if filename is None:
+    filename = os.path.dirname(os.path.realpath(sys.argv[0])) + "/" + "nasaearth.jpg"
+    
+    
+image = Image.open(filename).convert('RGB')
+
+for (x,y,z,block,theta) in egg(h=height,block=None,sphere=True):
+    mc.setBlock(x+pos.x,y+pos.y,z+pos.z,getPixel(image, (theta / (2*pi)) % 1, y / float(height), dither=None))
--- mcpipy/pysanka.py
+++ mcpipy/pysanka.py
@@ -3,9 +3,8 @@
 #
 
 #
-# pysanka.py [filename [height [oval|sphere|N]]]
+# pysanka.py [filename [height [oval|N]]]
 #  oval: wrap an oval image onto an egg
-#  sphere: wrap a rectangular image onto a sphere
 #  N: wrap a rectangular image onto an egg N times (N is an integer)
 #
 # Yeah, these arguments are a mess!
@@ -97,90 +96,85 @@ def getBestColor(rgb):
             bestColor = c
     return bestColor
 
-def getPixel(image, x, y, dither=False):
+def getPixel(image, x, y, dither=None):
     rgb = image.getpixel(( image.size[0]-1-floor( x * image.size[0] ), image.size[1]-1-floor( y * image.size[1] ) ))
-    if dither:
-        rgb[0] += uniform(-20,20)
-        rgb[1] += uniform(-20,20)
-        rgb[2] += uniform(-20,20)
+    if dither is not None:
+        tweaked = ( rgb[0] + uniform(-dither,dither), rgb[1] + uniform(-dither,dither), rgb[2] + uniform(-dither,dither) )
+        return getBestColor(tweaked)
     return getBestColor(rgb)[0:2]
 
-mc = Minecraft()
+if __name__ == '__main__':    
+    mc = Minecraft()
 
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-    if not os.path.isfile(filename):
-        filename = os.path.dirname(os.path.realpath(sys.argv[0])) + "/" + filename    
-else:
-    filename = os.path.dirname(os.path.realpath(sys.argv[0])) + "/" + "pysanka.jpg"
-
-if len(sys.argv) > 2:
-    height = int(sys.argv[2])
-else:
-    height = 100
-    
-oval = False
-sphereWrap = False
-    
-if len(sys.argv) > 3:
-    if sys.argv[3] == "oval":
-        oval = True
-    elif sys.argv[3] == "sphere":
-        sphereWrap = True
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        if not os.path.isfile(filename):
+            filename = os.path.dirname(os.path.realpath(sys.argv[0])) + "/" + filename    
     else:
-        repeat = int(sys.argv[3])
-else:
-    repeat = 2
+        filename = os.path.dirname(os.path.realpath(sys.argv[0])) + "/" + "pysanka.jpg"
 
-pos = mc.player.getPos()
+    if len(sys.argv) > 2:
+        height = int(sys.argv[2])
+    else:
+        height = 100
+        
+    oval = False
+    sphereWrap = False
+        
+    if len(sys.argv) > 3:
+        if sys.argv[3] == "oval":
+            oval = True
+        elif sys.argv[3] == "sphere":
+            sphereWrap = True
+        else:
+            repeat = int(sys.argv[3])
+    else:
+        repeat = 2
 
-if oval:
-    image = Image.open(filename).convert('RGBA')
+    pos = mc.player.getPos()
 
-    first = None
-    last = None
-    
-    start = [None] * image.size[1]
-    stop = [None] * image.size[1]
+    if oval:
+        image = Image.open(filename).convert('RGBA')
 
-    for y in range(image.size[1]):
-        for x in range(image.size[0]):
-            _,_,_,alpha = image.getpixel((x,y))
-            if alpha == 255:
-                start[y] = x
-                break
-        for x in range(image.size[0]-1,-1,-1):
-            _,_,_,alpha = image.getpixel((x,y))
-            if alpha == 255:
-                stop[y] = x
-                break
-        if start[y] is not None:
-            if first is None:
-                first = y    
-            last = y
-            
-    assert first is not None
+        first = None
+        last = None
+        
+        start = [None] * image.size[1]
+        stop = [None] * image.size[1]
 
-    for (x,y,z,block,theta) in egg(h=height,block=None):
-        imageY = first + int(float(height-1-y)/height*(last-first+1))
-        if imageY < first:
-            imageY = first
-        if imageY > last:
-            imageY = last
-        imageX = start[imageY]+ int((0.5 - 0.5 * sin(theta)) * (stop[imageY]-start[imageY]))
-        if imageX < start[imageY]:
-            imageX = start[imageY]
-        if imageX > stop[imageY]:
-            imageX = stop[imageY]
-        mc.setBlock(x+pos.x,y+pos.y,z+pos.z, getBestColor(image.getpixel((imageX,imageY))[0:3])[0:2])
-elif sphereWrap:
-    image = Image.open(filename).convert('RGB')
+        for y in range(image.size[1]):
+            for x in range(image.size[0]):
+                _,_,_,alpha = image.getpixel((x,y))
+                if alpha == 255:
+                    start[y] = x
+                    break
+            for x in range(image.size[0]-1,-1,-1):
+                _,_,_,alpha = image.getpixel((x,y))
+                if alpha == 255:
+                    stop[y] = x
+                    break
+            if start[y] is not None:
+                if first is None:
+                    first = y    
+                last = y
+                
+        assert first is not None
 
-    for (x,y,z,block,theta) in egg(h=height,block=None,sphere=True):
-        mc.setBlock(x+pos.x,y+pos.y,z+pos.z,getPixel(image, (theta / (2*pi)) % 1, y / float(height)))
-else:
-    image = Image.open(filename).convert('RGB')
+        for (x,y,z,block,theta) in egg(h=height,block=None):
+            imageY = first + int(float(height-1-y)/height*(last-first+1))
+            if imageY < first:
+                imageY = first
+            if imageY > last:
+                imageY = last
+            imageX = start[imageY]+ int((0.5 - 0.5 * sin(theta)) * (stop[imageY]-start[imageY]))
+            if imageX < start[imageY]:
+                imageX = start[imageY]
+            if imageX > stop[imageY]:
+                imageX = stop[imageY]
+            mc.setBlock(x+pos.x,y+pos.y,z+pos.z, getBestColor(image.getpixel((imageX,imageY))[0:3])[0:2])
+    else:
+        image = Image.open(filename).convert('RGB')
 
-    for (x,y,z,block,theta) in egg(h=height,block=None):
-        mc.setBlock(x+pos.x,y+pos.y,z+pos.z,getPixel(image, (theta * repeat / (2*pi)) % 1, y / float(height), dither=True))
+        for (x,y,z,block,theta) in egg(h=height,block=None):
+            mc.setBlock(x+pos.x,y+pos.y,z+pos.z,getPixel(image, (theta * repeat / (2*pi)) % 1, y / float(height)))
    
