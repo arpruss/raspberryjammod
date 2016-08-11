9c569b45a54073a7988f31a4a46044cfc3fb729b
arpruss <arpruss@gmail.com>
2016-08-10 20:47:42 -0500

    camera cleanup

--- mcpipy/camera.py
+++ mcpipy/camera.py
@@ -3,11 +3,16 @@ import pygame.camera
 import colors
 from time import sleep
 from sys import argv
+import random
+
+def clamp(x,a,b):
+    return min(b,max(x,a))
 
 mc = Minecraft()
 pos = mc.player.getTilePos()
 
-dither = True
+#dither = colors.DitheringMethod(rng=lambda:clamp(random.gauss(0,20),-40,40))
+dither = colors.DitheringMethod(rng=lambda:random.uniform(-20,20))
 width = 80
 if len(argv) >= 2:
    width = int(argv[1])
@@ -15,37 +20,21 @@ height = width * 3 // 4
 
 pygame.camera.init()
 camlist = pygame.camera.list_cameras()
+
 if camlist:
-   cam = pygame.camera.Camera(camlist[0],(640,480))
-   current = [[(-1,-1) for y in range(height)] for x in range(width)]
-   while True:
-       image = pygame.transform.scale(cam.get_image(), (width,height))
-       if not dither:
-           for x in range(width):
-               for y in range(height):
-                   block,_ = colors.rgbToBlock(image.get_at((x,y)))
-                   if current[x][y] != block:
-                       mc.setBlock(pos.x+x,pos.y+height-1-y,pos.z,block)
-                       current[x][y] = block
-       else:
-           pixels = [[list(image.get_at((x,y))[0:3]) for y in range(height)] for x in range(width)]
-           for x in range(width):
-               for y in range(height):
-                   block,actualRGB = colors.rgbToBlock(pixels[x][y])
-                   if current[x][y] != block:
-                       mc.setBlock(pos.x+x,pos.y+height-1-y,pos.z,block)
-                       current[x][y] = block
-                   for i in range(3):
-                       err = pixels[x][y][i] - actualRGB[i]
-                       if x + 1 < width:
-                           pixels[x+1][y][i] += err * 7 // 16
-                       if y + 1 < height:
-                           if 0 < x:
-                               pixels[x-1][y+1][i] += err * 3 // 16
-                           pixels[x][y+1][i] += err * 5 // 16
-                           if x + 1 < width:
-                               pixels[x+1][y+1][i] += err // 16
-       sleep(0.5)
+    cam = pygame.camera.Camera(camlist[0],(640,480))
+    current = [[AIR for y in range(height)] for x in range(width)]
+    random.seed(1234) # ensure same seed for each frame
+    while True:
+        image = pygame.transform.scale(cam.get_image(), (width,height))
+        
+        for x,y,block in colors.imageToBlocks(image.get_at, width, height, dither=dither):
+            if current[x][y] != block:
+                mc.setBlock(pos.x+x,pos.y+height-1-y,pos.z,block)
+                current[x][y] = block 
+                c+=1
+                
+        sleep(2)
 
 else:
    mc.postToChat('Camera not found')
