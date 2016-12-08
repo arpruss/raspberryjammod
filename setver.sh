f3e40a23128f549bdedae7a81eaddadd791e4468
arpruss <arpruss@gmail.com>
2016-12-08 12:56:16 -0600

    1.11

--- setver.sh
+++ setver.sh
@@ -1,2 +1,2 @@
 sed -i 's/public static final String VERSION [^;]*/public static final String VERSION = "'$1'"/' src/main/java/mobi/omegacentauri/raspberryjammod/RaspberryJamMod.java 19/src/main/java/mobi/omegacentauri/raspberryjammod/RaspberryJamMod.java
-sed -i 's/^version = "[^"]*"/version = "'$1'"/' build.gradle 19/build.gradle 194/build.gradle 110/build.gradle
+sed -i 's/^version = "[^"]*"/version = "'$1'"/' build.gradle 19/build.gradle 194/build.gradle 110/build.gradle 111/build.gradle
