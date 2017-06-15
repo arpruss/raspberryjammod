43593fd4c7cb6b13057e50db46ead4f60d81755d
arpruss <arpruss@gmail.com>
2017-06-15 07:46:24 -0500

    more keys

--- mcpipy/input.py
+++ mcpipy/input.py
@@ -121,7 +121,54 @@ if system() == 'Windows':
     MEDIA_PLAY_PAUSE = 0xB3
     BROWSER_BACK = 0xA6
     BROWSER_FORWARD = 0xA7
-
+    KEY_0 = 0x30
+    KEY_1 = 0x31
+    KEY_2 = 0x32
+    KEY_3 = 0x33
+    KEY_4 = 0x34
+    KEY_5 = 0x35
+    KEY_6 = 0x36
+    KEY_7 = 0x37
+    KEY_8 = 0x38
+    KEY_9 = 0x39
+    KEY_A = 0x41
+    KEY_B = 0x42
+    KEY_C = 0x43
+    KEY_D = 0x44
+    KEY_E = 0x45
+    KEY_F = 0x46
+    KEY_G = 0x47
+    KEY_H = 0x48
+    KEY_I = 0x49
+    KEY_J = 0x4A
+    KEY_K = 0x4B
+    KEY_L = 0x4C
+    KEY_M = 0x4D
+    KEY_N = 0x4E
+    KEY_O = 0x4F
+    KEY_P = 0x50
+    KEY_Q = 0x51
+    KEY_R = 0x52
+    KEY_S = 0x53
+    KEY_T = 0x54
+    KEY_U = 0x55
+    KEY_V = 0x56
+    KEY_W = 0x57
+    KEY_X = 0x58
+    KEY_Y = 0x59
+    KEY_Z = 0x5A
+    KEY_SEMICOLON = 0xBA
+    KEY_PLUS = 0xBB
+    KEY_COMMA = 0xBC
+    KEY_MINUS = 0xBD
+    KEY_PERIOD = 0xBE
+    KEY_SLASH = 0xBF
+    KEY_BACKQUOTE = 0xC0
+    KEY_OPEN_BRACKET = 0xDB
+    KEY_BACKSLASH = 0xDC
+    KEY_CLOSE_BRACKET = 0xDD
+    KEY_APOSTROPHE = 0xDE
+        
     def getPressState(key):
         v = windll.user32.GetAsyncKeyState(int(key))
         return bool(0x8000 & v), bool(0x0001 & v)
